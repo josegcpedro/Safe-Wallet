e5d1cc1898cd9e1ba712824afeafc3ec297298c0
Jose <pedrojcunha7@hotmail.com>
2025-09-03 15:40:15 +0200

    chore: choising background image

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,28 @@
 import { ImageBackground, Text, StyleSheet } from "react-native";
 
-const HomeImage = require("../assets/images/athene.jpg");
+const HomeImage = require("../assets/images/greece.jpg");
 
 export default function Index() {
   return (
     <ImageBackground
       source={HomeImage}
-      style={StyleSheet.absoluteFillObject}
+      style={styles.background}
       resizeMode="cover"
-    ></ImageBackground>
+    >
+      <Text style={styles.text}>Connecter</Text>
+    </ImageBackground>
   );
 }
 
+const styles = StyleSheet.create({
+  background: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+  },
+  text: {
+    color: "white",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 100,
+  }
+});
