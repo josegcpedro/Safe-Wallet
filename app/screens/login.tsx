790aaecd4268d2b7b6ae6106bb2c400849d77917
Jose <pedrojcunha7@hotmail.com>
2025-09-04 15:24:44 +0200

    feat: create whoAreWe page

--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -10,7 +10,6 @@ import { useRouter } from "expo-router";
 
 
 export default function Login() {
-    const HomeImage = require("../../assets/images/rome.jpg");
 
     const router = useRouter();
     const [email, setEmail] = useState('');
@@ -56,67 +55,66 @@ export default function Login() {
 
 
     return (
-        <ImageBackground
-            source={HomeImage}
-            style={styles.Background}
-            blurRadius={1}
-        >
-            <View style={styles.container}>
-                <Text style={styles.header}>Connectez vous ! </Text>
-                <KeyboardAvoidingView behavior="padding">
-                    <TextInput value={email} style={styles.input} placeholder="Email" placeholderTextColor="#888" autoCapitalize="none" onChangeText={(text) => setEmail(text)}></TextInput>
-                    <TextInput value={password} style={styles.input} placeholder="Password" placeholderTextColor="#888" autoCapitalize="none" onChangeText={(text) => setPassword(text)} secureTextEntry={true}></TextInput>
-                    {loading ? <ActivityIndicator size="large" color="#0000ff" />
-                        : <>
-                            <View style={styles.buttonRow}>
+        <View style={styles.container}>
+            <Text style={styles.header}>Connectez vous ! </Text>
+            <KeyboardAvoidingView behavior="padding">
+                <TextInput value={email} style={styles.input} placeholder="Email" placeholderTextColor="#888" autoCapitalize="none" onChangeText={(text) => setEmail(text)}></TextInput>
+                <TextInput value={password} style={styles.input} placeholder="Password" placeholderTextColor="#888" autoCapitalize="none" onChangeText={(text) => setPassword(text)} secureTextEntry={true}></TextInput>
+                {loading ? <ActivityIndicator size="large" color="#0000ff" />
+                    : <>
+                        <View style={styles.buttonRow}>
                             <Button style={styles.Button} onPress={signIn} >Login</Button>
                             <Button style={styles.Button} onPress={signUp} >Créer un compte</Button>
-                            </View>
-                        </>}
-                </KeyboardAvoidingView>
-            </View>
-        </ImageBackground>
+                        </View>
+                    </>}
+            </KeyboardAvoidingView>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    Background: {
-        ...StyleSheet.absoluteFillObject,
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    header: {
-        position: 'absolute',
-        top: 160,
-        fontSize: 30,
-        color: "white"
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        width: 350,
-        borderWidth: 0.5,
-        borderRadius: 10,
-        padding: 10,
-        backgroundColor: "#fff",
-        borderColor: "gray"
-    },
-    buttonRow: {
-        flexDirection: 'row',      
-        justifyContent: 'space-between', 
-        width: 350,                 
-        marginTop: 10,
-    },
-    Button: {
-        backgroundColor: "white",
-        flex: 1,       
-        marginHorizontal: 5,        
-    },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f3eeeaff",
+  },
+  header: {
+    position: 'absolute',
+    top: 165,
+    fontSize: 30,
+    color: "#333333",
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    width: 350,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#FFFFFF", 
+    borderColor: "#CCCCCC",
+    color: "#000000",
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 350,
+    marginTop: 10,
+  },
+  Button: {
+    flex: 1,
+    marginHorizontal: 5,
+    paddingVertical: 12,
+    borderRadius: 10,
+    backgroundColor: "#FFFFFF"
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
 
-})
---
+++ app/screens/whoAreWe.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function WhoAreWe(){
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
