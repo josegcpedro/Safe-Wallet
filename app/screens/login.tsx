d858da2d443cc0ff41b678d5097175e62444bdc7
Jose <pedrojcunha7@hotmail.com>
2025-09-04 12:02:32 +0200

    feat: setup logic on inputs

--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -1,19 +1,58 @@
 import { FIREBASE_AUTH } from "@/app/firebase/FireBaseConfig";
+import { Button } from "@react-navigation/elements";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react"
-import { TextInput, View } from "react-native";
+import { ActivityIndicator, TextInput, View } from "react-native";
 import { Text, StyleSheet } from "react-native";
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState('');
-    const auth = FIREBASE_AUTH
+    const [loading, setLoading] = useState(false);
+    const auth = FIREBASE_AUTH;
+
+    const signIn = async () => {
+        setLoading(true);
+        try{
+            const response = await signInWithEmailAndPassword(auth, email, password);
+            console.log(response);
+        } catch (error: any){
+            console.log(error);
+            alert('Sign in failed: ' + error.message)
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const signUp = async () => {
+        setLoading(true);
+        try{
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(response);
+            alert("check ur mails")
+        } catch (error: any){
+            console.log(error);
+            alert('Sign in failed: ' + error.message)
+        } finally {
+            setLoading(false);
+        }
+    }
+
+
     return (
         <View style={styles.container}>
             <TextInput value={email} style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={(text) => setEmail(text)}></TextInput>
             <TextInput value={password} style={styles.input} placeholder="Password" autoCapitalize="none" onChangeText={(text) => setPassword(text)} secureTextEntry={true}></TextInput>
 
+            { loading ? <ActivityIndicator size="large" color="#0000ff" />
+            :<>
+            <Button onPress={signIn} >Login</Button>
+            <Button onPress={signUp} >SignUp</Button>
+            </>}
+        
+        
         </View>
+
     )
 }
 
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
-
 const { getDefaultConfig } = require('expo/metro-config');
+
 const defaultConfig = getDefaultConfig(__dirname);
-DEFAULT_LOGGER_CONFIG.resolver.assetExts.push('cjs')
+
+defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
+
