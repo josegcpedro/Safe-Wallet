c76c63f6ab0be638a4875f28434bf25fa629a809
Jose <pedrojcunha7@hotmail.com>
2025-09-11 19:47:34 +0200

    feat: takes data from firebase db

--- app/tabs/accueil.tsx
+++ app/tabs/accueil.tsx
@@ -1,13 +1,65 @@
-import { Text, View, StyleSheet } from "react-native";
-import { FIREBASE_AUTH } from "@/src/firebase/FireBaseConfig";
+import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import { FIREBASE_AUTH, FIREBASE_DB } from "@/src/firebase/FireBaseConfig";
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import { useEffect, useState } from "react";
 
 export default function Accueil() {
-    const user = FIREBASE_AUTH.currentUser;
+    const [currentUid, setCurrentUid] = useState<string | null>(null);
+    const [userData, setUserData] = useState<any>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+
+
+    useEffect(() => {
+        const unsubscribe = FIREBASE_AUTH.onAuthStateChanged(user => { // quand un changement ce passe ça verifie si le user existe, si oui ça définit le currentUid comme user.uid
+            if (user) {
+                setCurrentUid(user.uid);
+            } else {
+                setCurrentUid(null);
+                setLoading(false);
+            }
+        });
+        return unsubscribe; // arrête de listener pour éviter les fuites de mémoire
+    }, []);
+
+    useEffect(() => {
+        if (currentUid) {
+            console.log("mis a jour");
+        }
+    }, [currentUid]);
+
+    useEffect(() => {
+        if (!currentUid) return;
+
+        const fetchData = async () => {
+            setLoading(true); // démarre le loader
+            try {
+                const docRef = doc(FIREBASE_DB, "users", currentUid); // essaye de récurer les données
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) { // si ya des données ça récupére
+                    const data = docSnap.data();
+                    console.log("Données Firestore :", data);
+                    setUserData(data);
+                } else {
+                    console.log("Pas encore de données pour cet utilisateur.");
+                    setUserData(null);
+                }
+            } catch (error) {
+                console.error("Erreur en récupérant les données :", error);
+            } finally {
+                setLoading(false); // arrête le loader quoi qu'il arrive
+            }
+        };
+        fetchData();
+    }, [currentUid]);
 
     return (
         <View style={styles.container}>
-            <Text style={styles.helloUser}>
-                Salut {user?.displayName} !
+            <Text>
+                <Text>Salary : {userData?.displayName ?? "Non renseigné"}</Text>
+
+
+                <Text>Salary : {userData?.salary ?? "Non renseigné"}</Text>
             </Text>
         </View>
     )
@@ -18,9 +70,5 @@ const styles = StyleSheet.create({
         backgroundColor: "#f3eeeaff",
         ...StyleSheet.absoluteFillObject,
         paddingTop: 100,
-    },
-    helloUser: {
-        fontSize:20,
-        paddingLeft:10,
     }
 })
